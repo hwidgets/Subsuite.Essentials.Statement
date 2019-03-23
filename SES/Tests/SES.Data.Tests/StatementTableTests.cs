e9911580be113530452a82c3716b6a20ba1c586d
Pierre Viara <viara@intechinfo.fr>
2019-03-23 13:41:46 +0100

    Added test on Statement deletion.

--- SES/Tests/SES.Data.Tests/StatementTableTests.cs
+++ SES/Tests/SES.Data.Tests/StatementTableTests.cs
@@ -1,4 +1,5 @@
 ﻿using CK.SqlServer;
+using Dapper;
 using FluentAssertions;
 using NUnit.Framework;
 using System;
@@ -49,6 +50,28 @@ namespace SES.Data.Tests
                 // #4. Inserts the aforesaid object and asserts that its id is greater than 1.
                 var sId = await sTable.CreateStatementAsync(ctx, systemId, stObj.Level, stObj.LanguageCode, stObj.Status, string.Empty);
                 Assert.That(sId, Is.GreaterThan(1));
+            }
+        }
+
+        [Test]
+        public async Task DeleteStatement_whenAlreadyExistingStatement_shouldNotThrowException_and_shouldDeleteForesaidStatement()
+        {
+            using (var ctx = new SqlStandardCallContext())
+            {
+                var sTable = CK.Core.StObjModelExtension.Obtain<StatementTable>(TestHelper.StObjMap.StObjs);
+
+                // #1. Creates the Statement object that is next going to be inserted in database.
+                var sId = await sTable.CreateStatementAsync(ctx, systemId, Level.Warning, 0, 0, string.Empty);
+
+                // #2. Deleting the foresaid Statement Level object should not present issues.
+                Func<Task> act = async () => await sTable.DeleteStatementAsync(ctx, systemId, sId);
+                act.Should().NotThrow<SqlDetailedException>();
+
+                // #3. Asserts that deletion process has been successful.
+                var stdObj = await ctx[sTable].Connection.QueryFirstOrDefaultAsync<StdStatementInfo>
+                    ("select * from SES.tStatement where StatementId = @id;", new { id = sId });
+                Assert.That(stdObj, Is.Null);
+
             }
         }
     }
